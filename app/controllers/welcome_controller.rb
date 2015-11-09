ea709c10b6d2188805ab2679cb87bd73c67b1c96
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-11-09 16:24:22 +0100

    changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ class WelcomeController < ApplicationController
   end
 
   def stl_file
-    # response.headers['Content-Type'] = 'application/vnd.ms-pki.stl'
-    send_file Rails.root.join("app", 'assets', '3d', params[:filename] + '.stl'), type: "application/vnd.ms-pki.stl"
+    send_file Rails.root.join('app', 'assets', '3d', params[:filename] + '.stl'), type: "application/vnd.ms-pki.stl"
   end
 end
