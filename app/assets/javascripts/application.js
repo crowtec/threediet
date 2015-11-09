5f7bcf5d31eea8f80781e654c541a7cc145e92c6
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-11-09 16:18:56 +0100

    changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require xml3d/xml3d
+//= require xml3d/camera
+//= require xml3d/xml3d-stl-plugin
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def stl_file
+    # response.headers['Content-Type'] = 'application/vnd.ms-pki.stl'
+    send_file Rails.root.join("app", 'assets', '3d', params[:filename] + '.stl'), type: "application/vnd.ms-pki.stl"
+  end
 end
